5346770f2937c30e53c239cbb9e280b705bae285
Aqib Javaid <aqibjavaid@Tintashs-MacBook-Pro.local>
2022-07-02 22:44:49 +0500

    Make changes in App.js file

--- src/App.js
+++ src/App.js
@@ -6,16 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>This project is created by Aqib Javaid</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Aqib Javaid (AJ)
+          This project is created as a demo to learn CI/CD Concept.
         </a>
       </header>
     </div>
